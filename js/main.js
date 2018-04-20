4af54d7f60100b149f4a953a3274f74a8bccb996
mikaelasquirchuk <mikaela@generalassemb.ly>
2018-04-20 20:58:41 +1000

    full screen

--- js/main.js
+++ js/main.js
@@ -1,29 +1,44 @@
 var iss_now_URL = "http://api.open-notify.org/iss-now.json";
-var issLocation;
+var issLocationNow;
 
 fetch(iss_now_URL, {method: "GET"})
     .then(function(rep){
         return rep.json();
     })
     .then(function(data){
-        var latitude = data.iss_position.latitude;
-        var latitudeAsInteger = parseInt(latitude, 10);
-        var longitude = data.iss_position.longitude;
-        var longitudeAsInteger = parseInt(longitude, 10);
-        issLocation = {lat: latitudeAsInteger, lng: longitudeAsInteger};
-        return issLocation;
+        var currentLatitude = data.iss_position.latitude;
+        var currentLatitudeAsInteger = parseInt(currentLatitude,10);
+        var currentLongitude = data.iss_position.longitude;
+        var currentLongitudeAsInteger = parseInt(currentLongitude, 10);
+        issLocationNow = {lat: currentLatitudeAsInteger, lng: currentLongitudeAsInteger};
+       return issLocationNow
     })
-    .then(function(issLocation){
+    .then(function(issLocationNow){
       function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 4,
-          center: issLocation
+          center: issLocationNow
         });
         var marker = new google.maps.Marker({
-          position: issLocation,
+          position: issLocationNow,
           map: map,
           icon: 'img/iss-icon-marker.png',
         });
+
+        // how to set polylines
+          // var issCoordinates = [
+          //   issLocation1,
+          //   issLocation2,
+          //   issLocation3
+          // ];
+          // var issPath = new google.maps.Polyline({
+          //   path: issCoordinates,
+          //   geodesic: true,
+          //   strokeColor: '#16161d',
+          //   strokeOpacity: 0.5,
+          //   strokeWeight: 3,
+          // });
+          // issPath.setMap(map);
       }
       initMap();
     })
